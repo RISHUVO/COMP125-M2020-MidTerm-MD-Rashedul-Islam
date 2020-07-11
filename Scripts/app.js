76dd5e5f62df87cc61a2a38fa203fb809996af06
RIS <misla237@my.centennialcollege.ca>
2020-07-10 20:01:00 -0400

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,30 @@
+// setup your IIFE (Immediately Invoked Function Expression)
+(function () {
+"use strict";
+
+// YOUR CODE GOES HERE
+
+    function Start()
+    {
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load", Start);
+
+    try 
+    
+    { var intro = document.getElementById ("IntroParagraph").value;
+    
+     {throw "Greece has 1,400 islands, though only 230 of them are inhabited. ";} 
+    
+    }
+
+
+
+
+
+
+
+})();
+
+
